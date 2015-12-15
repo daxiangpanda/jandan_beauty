e0ba893c0c06cb060836d34160c467d7c890f0d7
daxiangpanda <313514820@qq.com>
2015-12-15 13:01:40 +0800

    有时候会被拒绝 下次加入代理试试

--- fishc.py
+++ fishc.py
@@ -6,27 +6,27 @@ import os
 import time
 import random
 
+
 def url_open(url):
     req = urllib2.Request(url)
-    req.add_header('User-Agent',"Mozilla/5.0 (Linux; Android 4.4.4; en-us; Nexus 5 Build/JOP40D) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2307.2 Mobile Safari/537.36")
-
+    req.add_header('User-Agent',r'Mozilla/5.0 (Windows NT 5.2) AppleWebKit/534.30 (KHTML, like Gecko) Chrome/12.0.742.122 Safari/534.30')
     # proxies = ['120.195.198.69:80','120.195.195.249:80','112.64.28.11:8090']
     # proxy = random.choice(proxies)
     #
     # proxy_support = urllib2.ProxyHandler({'http':proxy})
     # opener = urllib2.build_opener(proxy_support)
     # urllib2.install_opener(opener)
-
     response = urllib2.urlopen(req)
     html = response.read()
     #print html
     return html
 
+
 def get_page(url):
     html = url_open(url).decode("utf-8")
 
     a = html.find('current-comment-page')+23
-    b = html.find(']',a)
+    b = html.find(']', a)
     return html[a:b]
 
 def find_imgs(url):
@@ -48,16 +48,17 @@ def find_imgs(url):
 
 def save_imgs(folder,img_addrs):
     for each in img_addrs:
+        if not each.startswith("http://ww"):
+            continue
         filename = each.split("/")[-1]
         with open(filename,'wb') as f:
             img = url_open(each)
             f.write(img)
 
-def download_mm(folder = "E:\\ooxx",pages = 10):
+def download_mm(folder = "E:\\ooxx",pages = 1000):
     if not os.path.exists(folder):
         os.mkdir(folder)
     os.chdir(folder)
-
     url = "http://i.jandan.net/ooxx"
     page_num = int(get_page(url))
 
@@ -68,7 +69,7 @@ def download_mm(folder = "E:\\ooxx",pages = 10):
         img_addrs = find_imgs(page_url)
         print page_url
         save_imgs(folder,img_addrs)
-        time.sleep(random.randint(0,5))
+        time.sleep(random.randint(1,7))
 
 if __name__ == '__main__':
     download_mm()
