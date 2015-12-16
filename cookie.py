3ec05fcb12d5c14ad57c84f601d1d1b87ab01340
daxiangpanda <313514820@qq.com>
2015-12-16 14:53:47 +0800

    反防爬方法： 1 使用多种UA 2 设置时间间隔 3 抓异常，重新运行请求html的代码
    下次争取能够把程序改成全自动化的程序

--- cookie.py
+++ cookie.py
@@ -1,19 +1,19 @@
 import os,sys
 import hashlib
 
-def CalcSha1(filepath):
-    with open(filepath,'rb') as f:
-        sha1obj = hashlib.sha1()
-        sha1obj.update(f.read())
-        hash = sha1obj.hexdigest()
-        print(hash)
-    return hash
-def CalcMD5(filepath):
-    with open(filepath,'rb') as f:
-        md5obj = hashlib.md5()
-        md5obj.update(f.read())
-        hash = md5obj.hexdigest()
-        print(hash)
-    return hash
-CalcSha1('E:/ooxx/0061Uxz8jw1epr7wjvjnvj30g10ngtdv.jpg')
-CalcMD5('E:/ooxx/0061Uxz8jw1epr7wjvjnvj30g10ngtdv.jpg')
+while True:
+    n = raw_input().split(" ")
+    if n != '':
+        a, b = n[0], n[1]
+    else:
+        break
+    s = a + b
+    output = []
+while True:
+    a = raw_input()
+    if a != '':
+        output.append(count[int(a)])
+    else:
+        break
+for i in output:
+    print i
